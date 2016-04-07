f72f6562eecadb962a8bb38a334465a5b831ce0c
Emma Taboada Barral <emma.taboada@babel.es>
2016-04-07 22:48:07 +0200

    grunt project

---
+++ GruntFile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+      development: {
+          files: {
+              "style.css": "less/style.less"
+          }
+      },
+      production: {
+          options: {
+            compress: true
+          },
+          files: {
+              "style.css": "less/style.less"
+          }
+      }
+    },
+
+    watch: {
+      styles: {
+        files: ['less/*.less'],
+        tasks: ['less:development'],
+        options: {
+          spawn: false,
+        }
+      }
+    }
+
+  });
+
+  // Loads the plugins
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['less:development', 'watch']);
+  grunt.registerTask('production', ['less:production']);
+
+};
