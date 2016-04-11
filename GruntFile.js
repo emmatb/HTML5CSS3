ae4c3ff38feb7926f0fa7d0bfe6658d01a2ebe32
Emma Taboada Barral <emma.taboada@babel.es>
2016-04-11 22:34:24 +0200

    config

--- GruntFile.js
+++ GruntFile.js
@@ -28,16 +28,30 @@ module.exports = function(grunt) {
           spawn: false,
         }
       }
-    }
+    },
 
+    browserSync: {
+     development: {
+       bsFiles: {
+        src : ['*.css', '*.html']
+      },
+     options: {
+        server: {
+            baseDir: "./"
+        },
+        watchTask: true
+      }
+    }
+  }
   });
 
   // Loads the plugins
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
 
   // Default task(s).
-  grunt.registerTask('default', ['less:development', 'watch']);
+  grunt.registerTask('default', ['less:development', 'browserSync', 'watch']);
   grunt.registerTask('production', ['less:production']);
 
 };
